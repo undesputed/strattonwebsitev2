a9e095cee2f3a5675cb94ed3d6c25ddaae2e6313
inksharedsolutions <webdev.inkshared@gmail.com>
2021-07-21 07:32:10 +0800

    updated with files needed

--- src/components/awards/index.js
+++ src/components/awards/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import deception from "../../assets/images/backgrounds/100-YEARS-OF-DECEPTION_web.jpg";
 import atlastians from "../../assets/images/backgrounds/ALASTIANS_WEB.jpg";
@@ -12,7 +12,7 @@ import {
 } from "react-bootstrap";
 
 
-const Item = ({item: {img, alt}}) => (
+const Item = ({ item: { img, alt } }) => (
     <Carousel.Item interval={1000}>
         <a className="thumbnail" href="javascript: void(0)">
             <img className="media-object"
@@ -23,22 +23,22 @@ const Item = ({item: {img, alt}}) => (
     </Carousel.Item>
 )
 
-  
-  const MyCarousel2 = ({ items }) => (
+
+const MyCarousel2 = ({ items }) => (
     <div className="root">
-      <Carousel controls={true} fade>
-        {items.map((item, i) =>
-                   <Carousel.Item key={i}>
-                      <a className="thumbnail" href="javascript:void(0)">
+        <Carousel controls={true} fade>
+            {items.map((item, i) =>
+                <Carousel.Item key={i}>
+                    <a className="thumbnail" href="javascript:void(0)">
                         <img className="media-object"
-                          src={item.img}
-                          alt={item.alt}
+                            src={item.img}
+                            alt={item.alt}
                         />
-                      </a>
-                    </Carousel.Item>)}
-      </Carousel>
+                    </a>
+                </Carousel.Item>)}
+        </Carousel>
     </div>
-  )
+)
 
 const ITEMS = [
     {
@@ -65,13 +65,13 @@ const ITEMS = [
 
 
 class Awards extends React.Component {
-    render(){
-      return (
-        <div>
-          <MyCarousel2 items={ITEMS} />
-        </div>
-      )
+    render() {
+        return (
+            <div>
+                <MyCarousel2 items={ITEMS} />
+            </div>
+        )
     }
-  }
+}
 
-  export default Awards;
+export default Awards;
